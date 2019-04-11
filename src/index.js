85439ea45183ed2c27e4b929631ad6f205da1052
anchit1501 <1501.anchit@gmail.com>
2019-04-11 18:20:20 +0530

    v0.1.1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
-import React,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Pagination from './Pagination';
 
-class App extends Component {
-    constructor(props){
-      super(props)
-      this.state=({
-        value: 1
-      });
-    }
-    
-    handlePageUpdate=(e)=>{
-      this.setState({
-        value:e
-      });
-      console.log('val',e)
-    }
-    render() {
-      return (
-        <div className="App">
-         <Pagination total={20}  nextBackPair={true} initialFinalPair={true} onPageChange={this.handlePageUpdate} pagePrompter={true}/>
-        </div>
-      );
-    }
-  }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Pagination />, document.getElementById('root'));
 
 
